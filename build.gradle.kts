2ccf3755fb5d69375d1ed6a0e9110430a2652395
Morningxxx <spechles@spechles.com>
2022-01-06 14:00:34 +0800

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+
+
+group = "com.spechles"
+version = "1.0-SNAPSHOT"
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+
+rootProject.name = "ma-jan"
+
+
+include(":start")
+
